3d745e5ef52991a0230f2007325c64ea14a92904
thiagoxpto <thiago.ntn@hotmail.com>
2025-04-28 23:57:13 -0300

    Iniciando projetos kafka para testes de performance.

---
+++ kafka-back-end/src/test/java/com/processor/async/KafkaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.processor.async;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
